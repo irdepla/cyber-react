c2a354ea6936d9854fe067817b82ebcbac66f952
irdepla <rixsiboyevislombek@gmail.com>
2025-01-03 15:55:39 +0500

    deployement failed

--- src/components/CartProduct/index.jsx
+++ src/components/CartProduct/index.jsx
@@ -1,48 +1,56 @@
 import React, { useState } from "react";
-import crossIcon from "../../assets/icons/cross-icon.svg"
+import crossIcon from "../../assets/icons/cross-icon.svg";
 import { useDispatch, useSelector } from "react-redux";
-import { decreaseQuantity, increaseQuantity, removeFromCart } from "../../store/cartSlice";
+import {
+  decreaseQuantity,
+  increaseQuantity,
+  removeFromCart,
+} from "../../store/cartSlice";
 
-const CartProduct = ({cartItem}) => {
-    const dispatch = useDispatch()    
-
+const CartProduct = ({ cartItem }) => {
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div
-        className="flex justify-between gap-[15px] w-[341px] py-4 pb-[56px] border-b-2"
-      >
+      <div className="flex justify-between gap-[15px] w-[341px] py-4 pb-[56px] border-b-2">
         <div className="item__img">
           <img className="w-[90px] h-[90px]" src={cartItem.product.image} alt="" />
         </div>
         <div className="item__info flex flex-col gap-2">
-          <h1>
-            {cartItem.product.name}
-          </h1>
+          <h1>{cartItem.product.name}</h1>
           <p className="font-semibold">In stock:{cartItem.product.stock}</p>
           <div className="flex items-center justify-between gap-6">
-
             <div className="flex justify-between items-center gap-2">
-            <button className="py-3 px-2" onClick={() => {
-                dispatch(decreaseQuantity(cartItem.product.id))
-                }}>-</button>
-            <span className="px-4 py-2 border-[0.5px] border-[#D9D9D9] rounded">{cartItem.quantity}</span>
-            <button className="py-3 px-2" onClick={() => {
-                dispatch(increaseQuantity(cartItem.product.id))
-                }}>+</button>
+              <button
+                className="py-3 px-2"
+                onClick={() => {
+                  dispatch(decreaseQuantity(cartItem.product.id));
+                }}
+              >
+                -
+              </button>
+              <span className="px-4 py-2 border-[0.5px] border-[#D9D9D9] rounded">
+                {cartItem.quantity}
+              </span>
+              <button
+                className="py-3 px-2"
+                onClick={() => {
+                  dispatch(increaseQuantity(cartItem.product.id));
+                }}
+              >
+                +
+              </button>
             </div>
-
 
             <p className="text-xl font-medium">${cartItem.product.price}</p>
 
-
-            <button onClick={() => {
-                dispatch(removeFromCart(cartItem.product.id))
-            }}>
-                <img className="w-6 h-6" src={crossIcon} alt="" />
+            <button
+              onClick={() => {
+                dispatch(removeFromCart(cartItem.product.id));
+              }}
+            >
+              <img className="w-6 h-6" src={crossIcon} alt="" />
             </button>
-
-
           </div>
         </div>
       </div>
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import CartProduct from '../../components/CartProduct';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import CartProduct from "../../components/CartProduct";
 
 const Cart = () => {
+  const cart = useSelector((store) => store.cart);
+  // const [sum, setSum] = useState(0)
+  let name = 0;
+  const [state, setstate] = useState();
 
-    const cart = useSelector((store) => store.cart)
-    // const [sum, setSum] = useState(0)
-    // let name = 0
-    // const [state, setstate] = useState();
+  return (
+    <>
+      <section className="cart">
+        <div className="container">
+          <h1 className="font-semibold text-2xl mt-10">Shopping Cart</h1>
+          <div className=" grid grid-cols-1 gap-10  mt-10">
+            {console.log(cart)}
+            {cart.length > 0 &&
+              cart.map((item) => <CartProduct key={item.id} cartItem={item} />)}
+          </div>
 
-    return (
-
-        <>
-        <section className='cart'>
-            <div className="container">
-                <h1 className='font-semibold text-2xl mt-10'>Shopping Cart</h1>
-            <div className=' grid grid-cols-1 gap-10  mt-10' key={cart.id}>
-            {
-                cart.map((item) => (
-                    <CartProduct key={item.product.id} cartItem={item} />
-                ))
-            }
-            </div>
-
-            <h2>Your total is:</h2>
-            <button>Order</button>
-            </div>
-            </section>
-        </>
-    );
-}
+          <h2>Your total is:</h2>
+          <button>Order</button>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Cart;
